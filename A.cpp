a4bbb1c107409a330ff97f6c7c9ca866f62affc7
Bill Lin <bill10391@gmail.com>
2023-12-24 11:26:17 -0500

    Pinely round 3

--- A.cpp
+++ A.cpp
@@ -21,5 +21,23 @@ int32_t main() {
         int n;
         cin >> n;
 
+        vector<int> quad(4);
+        for (int i = 0; i < n; i++) {
+            int x, y;
+            cin >> x >> y;
+
+            if (x >= 0) quad[0]++;
+            if (x <= 0) quad[1]++;
+            if (y >= 0) quad[2]++;
+            if (y <= 0) quad[3]++;
+        }
+
+        bool can = false;
+        for (int i = 0; i < 4; i++) {
+            if (quad[i] == n) can = true;
+        }
+        if (can) cout << "YES\n";
+        else cout << "NO\n";
+
     }
 }
--- B.cpp
+++ B.cpp
@@ -17,9 +17,31 @@ int32_t main() {
     int tt;
     cin >> tt;
 
+    vector<int> to_check;
+    int curr = 2;
+    for (int i = 1; curr <= 1e17; i++) {
+        to_check.push_back(curr);
+        curr *= 2;
+    }
+    to_check.push_back(3);
+
     while (tt--) {
         int n;
         cin >> n;
 
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        for (int k : to_check) {
+            set<int> s;
+            for (int i = 0; i < n; i++) {
+                s.insert(a[i] % k);
+            }
+
+            if (s.size() == 2) {
+                cout << k << '\n';
+                break;
+            }
+        }
     }
 }
